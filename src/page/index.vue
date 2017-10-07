8bf94663ee45f1b4499c353700b004710468b9ca
simbaHuang <hx19920411@hotmail.com>
2017-10-07 23:51:47 +0800

    首页

--- src/page/index.vue
+++ src/page/index.vue
@@ -58,6 +58,30 @@
         	</ul>
         </div>
 			</div>
+
+      <!-- 最新菜谱 -->
+      <div class="home-latest-recipes">
+        <!-- tit -->
+        <p class="con-tit">
+          <span>最新菜谱</span>
+          <a class="con-tit-btn" target="_blank" href="javascript:">更多菜单 &gt;</a>
+        </p>
+        <!-- 容器 -->
+        <div class="home-latest-container">
+          <!-- 内容 -->
+          <ul class="home-latest-list">
+            <li v-for="latest in latestList">
+              <a class="home-latest-con" href="javascript:">
+                <img class="home-latest-img" width="100%" height="100%" :src="latest.imgUrl" :alt="latest.name">
+                <!-- 显示信息 -->
+                <div class="">
+                  
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
   	</div>
 </template>
 
@@ -248,6 +272,85 @@ export default {
             }
           ]
         }
+      ],
+      latestList: [
+        {
+          name: '法式紫薯派',
+          author: '余甘果蜜',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '炸',
+          taste: '甜味',
+          imgUrl: 'http://site.meishij.net/r/174/213/2303424/a2303424_150737235953308.jpg'
+        },
+        {
+          name: '无蛋可可螺旋吐司',
+          author: '喜爱豆子',
+          comments: 5,
+          sentiment: 2394,
+          step: 9,
+          time: 15,
+          practice: '烘培',
+          taste: '其他口味',
+          imgUrl: 'http://site.meishij.net/r/75/127/4906825/a4906825_150737187413411.jpg'
+        },
+        {
+          name: '大闸蟹溜蛋',
+          author: '柔蓝水晶',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '炒',
+          taste: '家常味',
+          imgUrl: 'http://site.meishij.net/r/83/142/410583/a410583_150737118116813.jpg'
+        },
+        {
+          name: '苦瓜苹果饮',
+          author: '余甘果蜜',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '其他工艺',
+          taste: '其他口味',
+          imgUrl: 'http://site.meishij.net/r/174/213/2303424/a2303424_150737068931194.jpg'
+        },
+        {
+          name: '辣椒木耳炒蛋',
+          author: '心随彧动',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '炒',
+          taste: '香辣味',
+          imgUrl: 'http://site.meishij.net/r/216/197/6174466/a6174466_150736713955415.jpg'
+        },
+        {
+          name: '豉香蟹',
+          author: '飘雪的季节(来自腾讯的网友)',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '炒',
+          taste: '香辣味',
+          imgUrl: 'http://site.meishij.net/r/134/61/5140384/a5140384_150736675971749.jpg'
+        },
+        {
+          name: '麻酱佛手瓜丝莲藕卷',
+          author: '上海五香豆',
+          comments: 5,
+          sentiment: 16221,
+          step: 8,
+          time: 15,
+          practice: '煮',
+          taste: '家常味',
+          imgUrl: 'http://site.meishij.net/r/178/57/3764428/a3764428_150736514942427.jpg'
+        }
       ]
     }
   },
@@ -281,7 +384,7 @@ export default {
   		color: #ff3232;
   	}
   }
-	
+
 	/* 首页广告部分 */
   .home-advertise-container{
   	position: relative;
@@ -423,4 +526,24 @@ export default {
 			}
 		}
 	}
+
+  /* 最新菜谱 */
+  .home-latest-recipes{
+    @include con-auto;
+  }
+  .home-latest-list{
+    li{
+      width: 232.5px;
+      margin-right: 20px;
+      margin-bottom: 20px;
+      @include in-block;
+    }
+    li:nth-child(4n){
+      margin-right: 0;
+    }
+    .home-latest-img{
+      width: 232.5px;
+      height: 232.5px;
+    }
+  }
 </style>
