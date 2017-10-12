3dcd8acbee1086bf40b97be2ed9b08873218aa95
simbaHuang <hx19920411@hotmail.com>
2017-10-12 23:04:22 +0800

    菜单类型切换组件编辑

--- src/components/appRecipeClass.vue
+++ src/components/appRecipeClass.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="recipe-class">
+    <div class="recipe-class-tab">
+      <ul class="recipe-class-tab-list">
+        <li>家常菜谱</li>
+      </ul>
+    </div>
+    <div class="recipe-class-con">
+      <ul class="recipe-class-con-list">
+        <li></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -12,4 +22,26 @@
 <style lang="scss">
   @import '../style/base/common.scss';
 
+  .recipe-class{
+    margin-top: 20px;
+    @include con-auto;
+  }
+  .recipe-class-tab-list{
+    width: 100%;
+    font-size: 0;
+    background-color: rgba(255, 255, 255, .5);
+    li{
+      padding: 0 24px;
+      line-height: 50px;
+      font-size: 14px;
+      font-weight: 600;
+      color: #333;
+      cursor: pointer;
+      @include in-block;
+      &:hover{
+        color: #ff3232;
+        background: rgba(255, 255, 255, .5);
+      }
+    }
+  }
 </style>
